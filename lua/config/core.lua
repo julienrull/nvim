4d296d366e2096691d6e6589c46c4b6f05e7c9ad
julienrull <julien.rull38@gmail.com>
2026-01-28 09:25:32 +0100

    fix clipboard

--- lua/config/core.lua
+++ lua/config/core.lua
@@ -3,7 +3,8 @@ vim.opt.relativenumber = true
 vim.opt.expandtab = true -- Converts tabs to spaces
 vim.opt.numberwidth = 1
 vim.opt.tabstop = 4  -- Insert 4 spaces for a tab
-vim.opt.shiftwidth = 1 -- Change the number of space characters inserted for indentation
+vim.opt.shiftwidth = 4 -- Change the number of space characters inserted for indentation
+vim.opt.clipboard = "unnamedplus"
 
 vim.g.mapleader = " "
 vim.keymap.set('n', '<Leader>n', '<cmd>bnext<cr>')
